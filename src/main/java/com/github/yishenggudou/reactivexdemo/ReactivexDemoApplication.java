cc15f25919741ca7743253c864becedb1bfeed13
timger <yishenggudou@gmail.com>
2020-10-08 20:37:44 +0800

    init
    init

---
+++ src/main/java/com/github/yishenggudou/reactivexdemo/ReactivexDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.yishenggudou.reactivexdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivexDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactivexDemoApplication.class, args);
+    }
+
+}
